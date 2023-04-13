744694dbf02385b50c3d49851d10b518b906938b
Rajesh Reddy Thiyyagura <raj30498@gmail.com>
2023-04-13 18:31:28 -0400

    a

--- login.php
+++ login.php
@@ -1,74 +1,52 @@
 <?php
 session_start();
 $errors = array();
-
 // insert config.php
-include('config.php');
-include('header.php');
 
-if (isset($_POST['login']))
-{
-	$username = $_POST['username'];
-    $password = $_POST['password'];
-    
-    // get the salt from the database
-    $salt_query = "SELECT salt FROM users WHERE username = '$username'";
-    $salt_result = mysqli_query($db, $salt_query);
-    $row = mysqli_fetch_assoc($salt_result);
-    $salt = $row['salt'];
-    
-    // encrypt the input password using AES-256-CBC algorithm
-    $encrypted_password = openssl_encrypt($password, 'aes-256-cbc', $salt);
-    
-    // hash the encrypted input password using SHA-256 algorithm
-    $hashed_password = hash('sha256', $encrypted_password);
-    
-    // check if the username and hashed password match the database
-    $login_query = "SELECT * FROM users WHERE username = '$username' AND password = '$hashed_password'";
-    $login_result = mysqli_query($db, $login_query);
-    $row = mysqli_fetch_assoc($login_result);
-    
-    if($row) {
-        // login successful, set session variables
-        $_SESSION['username'] = $row['username'];
-        $_SESSION['id'] = $row['id'];
-        
-        // redirect to the dashboard page
-        header("Location: dashboard.php");
-        exit();
+include('config.php');
+ include('header.php');
+
+if (isset($_POST['login'])) {
+  $username = mysqli_real_escape_string($db, $_POST['username']);
+  $password = mysqli_real_escape_string($db, $_POST['password']);
+
+  if (empty($username)) { array_push($errors, "Username is required"); }
+  if (empty($password)) { array_push($errors, "Password is required"); }
+
+  if (count($errors) == 0) {
+    $password = md5($password);
+    $query = "SELECT * FROM users WHERE username='$username' AND password='$password'";
+    $results = mysqli_query($db, $query);
+    if (mysqli_num_rows($results) == 1) {
+      $_SESSION['username'] = $username;
+      $_SESSION['success'] = "You are now logged in";
+      header('location: index.php');
     } else {
-        // login failed
-        echo "Invalid username or password";
+      array_push($errors, "Wrong username/password combination");
     }
+  }
 }
 ?>
-
 <!DOCTYPE html>
 <html>
 <head>
   <title>Login - My Ecommerce Website</title>
-<style>
-	body{background-image:url("images/bake.jpg") ;}
-</style>
 </head>
 <body>
   <h1>Login</h1>
   <form method="post" action="login.php">
-	<?php include('errors.php'); ?>
-	<div>
-  	<label>Username</label>
-  	<input type="text" name="username" value="<?php echo $username; ?>">
-	</div>
-	<div>
-  	<label>Password</label>
-  	<input type="password" name="password">
-	</div>
-	<div>
-  	<button type="submit" name="login">Login</button>
-	</div>
-							
+    <?php include('errors.php'); ?>
+    <div>
+      <label>Username</label>
+      <input type="text" name="username" value="<?php echo $username; ?>">
+    </div>
+    <div>
+      <label>Password</label>
+      <input type="password" name="password">
+    </div>
+    <div>
+      <button type="submit" name="login">Login</button>
+    </div>
   </form>
-					<!--VGhlc2UgYXJlIG5vdCB0aGUgY3JlZGVudGlhbHMgeW91cmUgbG9va2luZyBmb3IsIEdvIHRvIHJvYm90cw---- >
 </body>
 </html>
-
--- register.php
+++ register.php
@@ -6,96 +6,84 @@ $errors = array();
 include('config.php');
 include('header.php');
 
-
 if (isset($_POST['register'])) {
-    $username = mysqli_real_escape_string($db, $_POST['username']);
-    $email = mysqli_real_escape_string($db, $_POST['email']);
-    $password_1 = mysqli_real_escape_string($db, $_POST['password_1']);
-    $password_2 = mysqli_real_escape_string($db, $_POST['password_2']);
+  $username = mysqli_real_escape_string($db, $_POST['username']);
+  $email = mysqli_real_escape_string($db, $_POST['email']);
+  $password_1 = mysqli_real_escape_string($db, $_POST['password_1']);
+  $password_2 = mysqli_real_escape_string($db, $_POST['password_2']);
 
-    if (empty($username)) {
-        array_push($errors, "Username is required");
+  // Validate username, email, and password
+  if (empty($username)) { 
+    array_push($errors, "Username is required"); 
+  }
+  if (empty($email)) { 
+    array_push($errors, "Email is required"); 
+  }
+  if (empty($password_1)) { 
+    array_push($errors, "Password is required"); 
+  }
+  if (strlen($password_1) < 8) {
+    array_push($errors, "Password must be at least 8 characters long");
+  }
+  if ($password_1 != $password_2) { 
+    array_push($errors, "Passwords do not match"); 
+  }
+
+  // Check if user already exists
+  $user_check_query = "SELECT * FROM users WHERE username='$username' OR email='$email' LIMIT 1";
+  $result = mysqli_query($db, $user_check_query);
+  $user = mysqli_fetch_assoc($result);
+    
+  if ($user) {
+    if ($user['username'] === $username) {
+      array_push($errors, "Username already exists");
     }
-    if (empty($email)) {
-        array_push($errors, "Email is required");
+
+    if ($user['email'] === $email) {
+      array_push($errors, "Email already exists");
     }
-    if (empty($password_1)) {
-        array_push($errors, "Password is required");
-    }
-    if ($password_1 != $password_2) {
-        array_push($errors, "Passwords do not match");
-    }
+  }
 
-    $user_check_query = "SELECT * FROM users WHERE username='$username' OR email='$email' LIMIT 1";
-    $result = mysqli_query($db, $user_check_query);
-    $user = mysqli_fetch_assoc($result);
-
-    if ($user) {
-        if ($user['username'] === $username) {
-            array_push($errors, "Username already exists");
-        }
-
-        if ($user['email'] === $email) {
-            array_push($errors, "Email already exists");
-        }
-    }
-
-    if (count($errors) == 0) {
-        // generate a random salt
-        $salt = openssl_random_pseudo_bytes(32);
-
-        // encrypt the password using AES-256-CBC algorithm
-        $encrypted_password = openssl_encrypt($password_2, 'aes-256-cbc', $salt);
-
-        // hash the encrypted password using SHA-256 algorithm
-        $hashed_password = hash('sha256', $encrypted_password);
-
-        // prepare the SQL statement
-        $sql = "INSERT INTO users (username, email, password, salt) VALUES ('$username', '$email', '$hashed_password', '$salt')";
-
-        // execute the statement
-        if(mysqli_query($db, $sql)) {
-            // registration successful
-            echo "Registration successful";
-        } else {
-            // registration failed
-            echo "Registration failed";
-        }
-
-        $_SESSION['username'] = $username;
-        $_SESSION['success'] = "You are now logged in";
-        header('location: index.php');
-    }
+  // If no errors, create user
+  if (count($errors) == 0) {
+    $password = md5($password_1);
+    $query = "INSERT INTO users (username, email, password) VALUES ('$username', '$email', '$password')";
+    mysqli_query($db, $query);
+    $_SESSION['username'] = $username;
+    $_SESSION['success'] = "You are now logged in";
+    header('location: index.php');
+  }
 }
 ?>
-    <!DOCTYPE html>
-    <html>
-    <head>
-      <title>Register - My Ecommerce Website</title>
-    </head>
-    <body>
-      <h1>Register</h1>
-      <form method="post" action="register.php">
-        
-        <div>
-          <label>Username</label>
-          <input type="text" name="username" value="<?php echo $username; ?>">
-        </div>
-        <div>
-          <label>Email</label>
-          <input type="email" name="email" value="<?php echo $email; ?>">
-        </div>
-        <div>
-          <label>Password</label>
-          <input type="password" name="password_1">
-        </div>
-        <div>
-          <label>Confirm Password</label>
-          <input type="password" name="password_2">
-        </div>
-        <div>
-          <button type="submit" name="register">Register</button>
-        </div>
-      </form>
-    </body>
-    </html>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Register - My Ecommerce Website</title>
+</head>
+<body>
+  <h1>Register</h1>
+  <form method="post" action="register.php">
+    <?php include('errors.php'); ?>
+    <div>
+      <label>Username</label>
+      <input type="text" name="username" value="<?php echo $username; ?>">
+    </div>
+    <div>
+      <label>Email</label>
+      <input type="email" name="email" value="<?php echo $email; ?>">
+    </div>
+    <div>
+      <label>Password</label>
+      <input type="password" name="password_1">
+    </div>
+    <div>
+      <label>Confirm Password</label>
+      <input type="password" name="password_2">
+    </div>
+    <div>
+      <button type="submit" name="register">Register</button>
+    </div>
+  </form>
+</body>
+</html>
