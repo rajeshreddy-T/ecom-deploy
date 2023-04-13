029cb1943d7fa073bacf830b59e2e08b4dea97d3
Rajesh Reddy Thiyyagura <raj30498@gmail.com>
2023-04-13 18:25:19 -0400

    a

--- register.php
+++ register.php
@@ -4,75 +4,70 @@ $errors = array();
 // insert config.php
 
 include('config.php');
- include('header.php');
+include('header.php');
 
 
 if (isset($_POST['register'])) {
-  $username = mysqli_real_escape_string($db, $_POST['username']);
-  $email = mysqli_real_escape_string($db, $_POST['email']);
-  $password_1 = mysqli_real_escape_string($db, $_POST['password_1']);
-  $password_2 = mysqli_real_escape_string($db, $_POST['password_2']);
+    $username = mysqli_real_escape_string($db, $_POST['username']);
+    $email = mysqli_real_escape_string($db, $_POST['email']);
+    $password_1 = mysqli_real_escape_string($db, $_POST['password_1']);
+    $password_2 = mysqli_real_escape_string($db, $_POST['password_2']);
 
-  if (empty($username)) { array_push($errors, "Username is required"); }
-  if (empty($email)) { array_push($errors, "Email is required"); }
-  if (empty($password_1)) { array_push($errors, "Password is required"); }
-  if ($password_1 != $password_2) { array_push($errors, "Passwords do not match"); }
+    if (empty($username)) {
+        array_push($errors, "Username is required");
+    }
+    if (empty($email)) {
+        array_push($errors, "Email is required");
+    }
+    if (empty($password_1)) {
+        array_push($errors, "Password is required");
+    }
+    if ($password_1 != $password_2) {
+        array_push($errors, "Passwords do not match");
+    }
 
     $user_check_query = "SELECT * FROM users WHERE username='$username' OR email='$email' LIMIT 1";
     $result = mysqli_query($db, $user_check_query);
     $user = mysqli_fetch_assoc($result);
-    
+
     if ($user) {
-    if ($user['username'] === $username) {
-    array_push($errors, "Username already exists");
+        if ($user['username'] === $username) {
+            array_push($errors, "Username already exists");
+        }
+
+        if ($user['email'] === $email) {
+            array_push($errors, "Email already exists");
+        }
     }
 
-    if ($user['email'] === $email) {
-        array_push($errors, "Email already exists");
-      }
-    }
+    if (count($errors) == 0) {
+        // generate a random salt
+        $salt = openssl_random_pseudo_bytes(32);
 
-    if (count($errors) == 0) {    
-      
-      // generate a random salt
-      $salt = openssl_random_pseudo_bytes(32);
-      
-      // encrypt the password using AES-256-CBC algorithm
-      $encrypted_password = openssl_encrypt($password_2, 'aes-256-cbc', $salt);
-      
-      // hash the encrypted password using SHA-256 algorithm
-      $hashed_password = hash('sha256', $encrypted_password);
-      
-      // prepare the SQL statement
-      $sql = "INSERT INTO users (username, email, password, salt) VALUES ('$username', '$email', '$hashed_password', '$salt')";
-      
-      // execute the statement
-      if(mysqli_query($db, $sql)) {
-          // registration successful
-          echo "Registration successful";
-      } else {
-          // registration failed
-          echo "Registration failed";
-      }
-    // // generate a random salt
-    // $salt = openssl_random_pseudo_bytes(32);
-    
-    // // encrypt the password using AES-256-CBC algorithm
-    // $encrypted_password = openssl_encrypt($password_2, 'aes-256-cbc', $salt);
-    
-    // // hash the encrypted password using SHA-256 algorithm
-    // $hashed_password = hash('sha256', $encrypted_password);
+        // encrypt the password using AES-256-CBC algorithm
+        $encrypted_password = openssl_encrypt($password_2, 'aes-256-cbc', $salt);
 
-    // $query = "INSERT INTO users (username, email, password,salt) VALUES ('$username', '$email', '$password','$salt')";
-    
-    // mysqli_query($db, $query);
-    $_SESSION['username'] = $username;
-    $_SESSION['success'] = "You are now logged in";
-    header('location: index.php');
+        // hash the encrypted password using SHA-256 algorithm
+        $hashed_password = hash('sha256', $encrypted_password);
+
+        // prepare the SQL statement
+        $sql = "INSERT INTO users (username, email, password, salt) VALUES ('$username', '$email', '$hashed_password', '$salt')";
+
+        // execute the statement
+        if(mysqli_query($db, $sql)) {
+            // registration successful
+            echo "Registration successful";
+        } else {
+            // registration failed
+            echo "Registration failed";
+        }
+
+        $_SESSION['username'] = $username;
+        $_SESSION['success'] = "You are now logged in";
+        header('location: index.php');
     }
 }
 ?>
-    
     <!DOCTYPE html>
     <html>
     <head>
