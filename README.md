1db2c793908b48780172587f4a499008db05d634
Rajesh Reddy Thiyyagura <raj30498@gmail.com>
2023-04-13 15:55:21 -0400

    first commit

--- README.md
+++ README.md
@@ -41,3 +41,4 @@ This is a simple ecommerce website with plain php. It has a simple admin panel w
 # ecom-deploy
 # ecom-deploy
 # ecom-deploy
+# ecom-deploy
--- config.php
+++ config.php
@@ -3,8 +3,8 @@
 // Path: config.php
 // DB credentials.
 define('DB_HOST','localhost');
-define('DB_USER','wordpressuser');
-define('DB_PASS','password');
+define('DB_USER','project2user');
+define('DB_PASS','raja');
 define('DB_NAME','project2');
 // Establish database connection.
 $db = mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME);
